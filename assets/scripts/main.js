e2c6dbd173e50fbdbfb5b13f8d24efa980bc94e6
e_kaikei <e_kaikei@protonmail.com>
2021-03-21 21:59:09 +0900

    Can't figure the way to for loop in gin.Tukareta..

--- assets/scripts/main.js
+++
@@ -1,6 +0,0 @@
-let drinkNum = document.querySelector('.drinkNum');
-
-selectElement.addEventListener('change', (event) => {
-  const result = document.querySelector('.Drinks');
-  result.textContent = `The number of drinks are ${event.target.value}`;
-});
